3fadeb654cbbbb9b0d79323a2cdf6ac457a3e113
Dennis Wright <88091683+Dennis-Wright@users.noreply.github.com>
2026-01-18 20:48:24 +0000

    Update utils.py
    Added utils functions that are used across multiple files

--- 2026/Vault System/utils.py
+++ 2026/Vault System/utils.py
@@ -1,1 +1,15 @@
+import os
 
+def get_width():
+    try:
+        width = os.get_terminal_size().columns
+    except OSError:
+        width = 80
+
+    return width
+# End function
+
+def get_padding(terminal_width, text):
+    left_padding = max((terminal_width - text) // 2, 0)
+
+    return left_padding
